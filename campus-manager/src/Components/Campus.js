0ed989dd0b187ad9ffb24ac80db1934e1d7f1971
Shaina Lowenthal <shaina.lowenthal96@myhunter.cuny.edu>
2019-06-05 10:05:50 -0400

    changed no student message

--- campus-manager/src/Components/Campus.js
+++ campus-manager/src/Components/Campus.js
@@ -8,46 +8,36 @@ class Campus extends Component{
     constructor(props){
         super(props);
         this.state =
-        {Name: 'name',
-        Image: 'image',
-        Address: 'address',
-        Description: 'description',
-        Students: 'students',
-        mess: true
+        {
+        hasStudent: false
     };
 
     }
     render(){
-    if(this.state.mess)
-    {
+   
         return<body>
-            <h1>CAMPUS INFO</h1>
-            <div>  Name: {this.props.name}</div>
+            <h1>CAMPUS INFO  </h1>
+            <div class ="campus"></div>
+            <div>Name: {this.props.name}</div>
+           {/* <img src={this.props.image}>Image: </img> */}
            <div>Image: {this.props.image}</div>
             <div>Address: {this.props.address}</div>
             <div> Description: {this.props.description}</div>
-            <div>  Students on campus: {this.props.students} onChange = {this.toggleHidden.bind(this)}</div></body>;
-    }
-    else 
-    {
-        return<body>
-            <h1>CAMPUS INFO</h1>
-            <div>  Name: {this.props.name}</div>
-           <div>Image: {this.state.image}</div>
-            <div>Address: {this.props.address}</div>
-            <div> Description: {this.props.description}</div>
-            <div>There are no students currently registered to this campus.</div></body>;
-    }
+            <div>  Students on campus: {!this.props.hasStudent ?
+            "This campus does not have any students." :this.props.student}</div>
+            </body>;
+    
+   
     
     }
 }
 Campus.propTypes = {
     Name: PropTypes.string.isRequired,
-    Image: PropTypes.string.isRequired,
+    Image: PropTypes.string,
     Address: PropTypes.string.isRequired,
     Sescription: PropTypes.string.isRequired,
     Students: PropTypes.objectOf(PropTypes.students),
-    mess: PropTypes.bool
+    hasStudent: PropTypes.bool
 }
 export default Campus;
 
